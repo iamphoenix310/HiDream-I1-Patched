4d4dd93bbf91d3484feb625ec4acb9342aee6483
“Pankaj <pankaj@overallsite.com>
2025-04-12 06:13:34 +0530

    h

--- hi_diffusers/models/attention_processor.py
+++ hi_diffusers/models/attention_processor.py
@@ -2,6 +2,5 @@ from typing import Optional
 import torch
 from .attention import HiDreamAttention
 from .attention_processor_torch import HiDreamAttnProcessor_torch
-
-# ⚠️ Patch: Disable flash-attn by aliasing torch-based fallback
 HiDreamAttnProcessor_flashattn = HiDreamAttnProcessor_torch
+
