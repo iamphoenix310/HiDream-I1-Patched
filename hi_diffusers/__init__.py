966e9a7e5792528c96e5a862f3a6512274722a6b
Pankaj Sharma <8818473+iamphoenix310@users.noreply.github.com>
2025-04-12 06:44:14 +0530

    Update __init__.py

--- hi_diffusers/__init__.py
+++ hi_diffusers/__init__.py
@@ -1,1 +1,5 @@
-from .pipelines.hidream_image.pipeline_hidream_image import HiDreamImagePipeline
+# hi_diffusers/__init__.py
+
+# Only import core interfaces, not deep dependencies that may recurse
+from .pipelines.hidream_image import pipeline_hidream_image
+from .models.transformers import transformer_hidream_image
